6322cfb02a8d9d3788537e038d77fb8cbd77f527
Tamal Saha <tamal@appscode.com>
2018-06-06 11:26:36 -0700

    added-all

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"io/ioutil"
+	"log"
+	"github.com/ghodss/yaml"
+	"fmt"
+)
 
+func main() {
+	data, err := ioutil.ReadFile("pod.yaml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var pod Pod
+	err = yaml.Unmarshal(data, &pod)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(pod.Kind)
 }
