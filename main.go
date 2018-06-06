1c5ea214b6fef4ecad6de71c87ee5d32a4ccba4b
Tamal Saha <tamal@appscode.com>
2018-06-06 11:40:01 -0700

    added-all

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"github.com/ghodss/yaml"
 	"fmt"
+	"strings"
+	"os"
 )
 
 func main() {
@@ -19,5 +21,35 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(pod.Kind)
+	cmds := pod.Spec.Containers[0].Command
+
+	for _, c := range cmds {
+		if strings.Contains(c, "kube-apiserver") {
+			fields := strings.Fields(c)
+
+			var idx int
+			for i, w := range fields {
+				if strings.HasSuffix(w, "kube-apiserver") {
+					idx = i
+					break
+				}
+			}
+
+			var args []string
+
+			for _, w := range fields[idx+1:] {
+
+
+
+			}
+
+			
+			fmt.Println(strings.Join(fields, "\n"))
+			os.Exit(0)
+		}
+	}
+
+	fmt.Println(pod.Spec.Containers[0].Command)
+
+	// kube-apiserver
 }
